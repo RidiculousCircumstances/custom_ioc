545b4df6f8f79b4742f0fa618e62e2ff93728d40
Ivan Kozlov <103379782+RidiculousCircumstances@users.noreply.github.com>
2022-09-11 17:47:44 +0700

    initial implementationt

---
+++ src/decors.ts
@@ -0,0 +1,33 @@
+import 'reflect-metadata';
+import { v4 } from 'uuid'
+
+export function Injectable() {
+	return function (target: Function) {
+
+		const deps = Reflect.getMetadataKeys(target);
+		let tokens: Array<symbol> = [];
+		for (const dep of deps) {
+			const token = Reflect.getMetadata(dep, target);
+			tokens.push(token);
+			Reflect.deleteMetadata(dep, target)
+		}
+		tokens.shift();
+
+		const depsTree = {
+			hostId: target.name,
+			tokens
+		};
+
+		Reflect.defineMetadata('dependencies', depsTree, target)
+		
+	};
+}
+
+export function Inject(token: string | symbol) {//готов!
+	return function (target: Object, propertyKey: string | symbol, parameterIndex: number) {
+		const tokenWithPosition = token;
+		
+		Reflect.defineMetadata(String(v4()), tokenWithPosition, target);
+		
+	}
+}
