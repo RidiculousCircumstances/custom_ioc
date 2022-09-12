7c05efbd5e968f36cfa1189b0ef03f7758e237ad
Ivan Kozlov <103379782+RidiculousCircumstances@users.noreply.github.com>
2022-09-12 17:46:00 +0700

    formattin performedg

--- src/decors.ts
+++ src/decors.ts
@@ -1,33 +1,30 @@
 import 'reflect-metadata';
-import { v4 } from 'uuid'
+import { v4 } from 'uuid';
 
 export function Injectable() {
-	return function (target: Function) {
+  return function (target: Function) {
+    const deps = Reflect.getMetadataKeys(target);
+    let tokens: Array<symbol> = [];
+    for (const dep of deps) {
+      const token = Reflect.getMetadata(dep, target);
+      tokens.push(token);
+      Reflect.deleteMetadata(dep, target);
+    }
+    tokens.shift();
 
-		const deps = Reflect.getMetadataKeys(target);
-		let tokens: Array<symbol> = [];
-		for (const dep of deps) {
-			const token = Reflect.getMetadata(dep, target);
-			tokens.push(token);
-			Reflect.deleteMetadata(dep, target)
-		}
-		tokens.shift();
+    const depsTree = {
+      hostId: target.name,
+      tokens,
+    };
 
-		const depsTree = {
-			hostId: target.name,
-			tokens
-		};
-
-		Reflect.defineMetadata('dependencies', depsTree, target)
-		
-	};
+    Reflect.defineMetadata('dependencies', depsTree, target);
+  };
 }
 
 export function Inject(token: string | symbol) {
-	return function (target: Object, propertyKey: string | symbol, parameterIndex: number) {
-		const tokenWithPosition = token;
-		
-		Reflect.defineMetadata(String(v4()), tokenWithPosition, target);
-		
-	}
+  return function (target: Object, propertyKey: string | symbol, parameterIndex: number) {
+    const tokenWithPosition = token;
+
+    Reflect.defineMetadata(String(v4()), tokenWithPosition, target);
+  };
 }
--- src/ioc.ts
+++ src/ioc.ts
@@ -2,61 +2,53 @@ import { Newable } from './type.interface';
 import 'reflect-metadata';
 
 interface depsInterface {
-	hostId: string,
-	tokens: symbol[]
+  hostId: string;
+  tokens: symbol[];
 }
 
 export class Container {
-	private static instances = new Map<string | symbol, any>();
-	private static bindingMap = new Map<string | symbol, Newable<any>>();
+  private static instances = new Map<string | symbol, any>();
+  private static bindingMap = new Map<string | symbol, Newable<any>>();
 
-	static bind<T>(token: string | symbol, classToInject: Newable<T>, scope: 'singleton' | 'common' = 'singleton') {
-		Reflect.defineMetadata('scope', scope, classToInject);
-		Container.bindingMap.set(token, classToInject);
-	}
+  static bind<T>(
+    token: string | symbol,
+    classToInject: Newable<T>,
+    scope: 'singleton' | 'common' = 'singleton',
+  ) {
+    Reflect.defineMetadata('scope', scope, classToInject);
+    Container.bindingMap.set(token, classToInject);
+  }
 
-	static unbind(token: string | symbol) {
-		if (Container.bindingMap.has(token)) {
-			Container.bindingMap.delete(token);
+  static unbind(token: string | symbol) {
+    if (Container.bindingMap.has(token)) {
+      Container.bindingMap.delete(token);
 
-			if (Container.instances.has(token)) {
-				Container.instances.delete(token);
-			}
-		
-		} else {
-			throw new Error('There are no objects with specified token to unbind');
-		}
+      if (Container.instances.has(token)) {
+        Container.instances.delete(token);
+      }
+    } else {
+      throw new Error('There are no objects with specified token to unbind');
+    }
+  }
 
-	}
-	
-	static resolve<T>(token: string | symbol): T {
-
-		const template: Newable<T> = Container.bindingMap.get(token);
-		if (!template) {
-			throw new Error(`Container does not contain an object with token ${String(token)}`);
-		}
-		const scope = Reflect.getMetadata('scope', template);
-		if (Container.instances.has(token)) {
-			return Container.instances.get(token);
-		}
-		const depsInfo: depsInterface = Reflect.getMetadata("dependencies", template);
-		const injections = depsInfo.tokens.map((t): any => {
-			return Container.resolve(t);
-		});
-		const instance = new template(...injections.reverse());
-		if (scope != 'singleton') {
-			return instance; 
-		}
-		Container.instances.set(token, instance);
-		return instance; 		
-	}
+  static resolve<T>(token: string | symbol): T {
+    const template: Newable<T> = Container.bindingMap.get(token);
+    if (!template) {
+      throw new Error(`Container does not contain an object with token ${String(token)}`);
+    }
+    const scope = Reflect.getMetadata('scope', template);
+    if (Container.instances.has(token)) {
+      return Container.instances.get(token);
+    }
+    const depsInfo: depsInterface = Reflect.getMetadata('dependencies', template);
+    const injections = depsInfo.tokens.map((t): any => {
+      return Container.resolve(t);
+    });
+    const instance = new template(...injections.reverse());
+    if (scope != 'singleton') {
+      return instance;
+    }
+    Container.instances.set(token, instance);
+    return instance;
+  }
 }
-
-
-
-
-
-
-
-
-
